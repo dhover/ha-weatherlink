de37995e6f3a0e306f87323c29b849a92dd3efa8
Dinant Scholte in 't Hoff <dinant@dtwm.nl>
2025-06-04 16:49:35 +0200

    change __init__

--- custom_components/ha-weatherlink/__init__.py
+++ custom_components/ha-weatherlink/__init__.py
@@ -1,20 +1,26 @@
 from .const import DOMAIN
+from .coordinator import WeatherlinkCoordinator
 
 PLATFORMS = ["sensor"]
 
 
 async def async_setup(hass, config):
-    hass.data[DOMAIN] = {}
+    hass.data.setdefault(DOMAIN, {})
     return True
 
 
 async def async_setup_entry(hass, entry):
+    # Create and store the coordinator
+    coordinator = WeatherlinkCoordinator(hass, entry.data["host"])
+    await coordinator.async_config_entry_first_refresh()
+    hass.data[DOMAIN][entry.entry_id] = coordinator
+
     # Forward the config entry setup to the sensor platform
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
 
 async def async_unload_entry(hass, entry):
-    # Unload the sensor platform
     await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+    hass.data[DOMAIN].pop(entry.entry_id)
     return True
